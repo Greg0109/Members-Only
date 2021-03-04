b898165c8e1f3de2b50544a0d9f5a1d828acc3fc
Greg0109 <greg.rabago@gmail.com>
2021-03-04 16:31:06 +0100

    Add name and username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_151749) do
+ActiveRecord::Schema.define(version: 2021_03_04_152644) do
 
   create_table "stories", force: :cascade do |t|
     t.text "title"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_151749) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "name"
+    t.text "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
