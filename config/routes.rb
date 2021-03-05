c0461ffafb86df9628efeb44b74a9d5808cb832d
Greg0109 <greg.rabago@gmail.com>
2021-03-05 09:21:17 +0100

    Finish project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: "registrations" }
+  devise_scope :user do #To make sure it doesn't fail
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :stories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "stories#index"
