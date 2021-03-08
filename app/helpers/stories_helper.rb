97387900e4ba6c742a79a609dfbe2a197a41a410
Greg0109 <greg.rabago@gmail.com>
2021-03-08 08:56:48 +0100

    Fix linter errors

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,27 +1,24 @@
-# rubocop:disable Layout/EndOfLine
+# rubocop:disable Style/GuardClause
 
 module StoriesHelper
-    def display_name(user)
-        if user_signed_in?
-            user.username = user.username
-        else
-            user.username = "Anonymous"
-        end
-    end
+  def display_name(user)
+    user.username = if user_signed_in?
+                      user.username
+                    else
+                      'Anonymous'
+                    end
+  end
 
-    def display_button(user,story)
-        if user_signed_in?
-            if current_user.username = user.username
-                link_to 'Delete', story, method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-danger p-1 text-white'
-            end
-        end
+  def display_button(user, story)
+    if user_signed_in? && (current_user.username = user.username)
+      link_to 'Delete', story, method: :delete, data: { confirm: 'Are you sure?' },
+                               class: 'btn btn-danger p-1 text-white'
     end
+  end
 
-    def display_new_story_button
-        if user_signed_in?
-            link_to 'New story', new_story_path, class: 'btn bg-dark px-5 py-0 text-white'
-        end
-    end
+  def display_new_story_button
+    link_to 'New story', new_story_path, class: 'btn bg-dark px-5 py-0 text-white' if user_signed_in?
+  end
 end
 
-# rubocop:enable Layout/EndOfLine
+# rubocop:enable Style/GuardClause
